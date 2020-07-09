62fb130b2a2d4e16b6520115aa193dee31a50820
KOLANICH <kolan_n@mail.ru>
2020-07-09 13:37:47 +0300

    Moved the metadata into setup.cfg

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 """
 <Program Name>
@@ -60,56 +60,6 @@
 """
 
 from setuptools import setup
-from setuptools import find_packages
 
-
-with open('README.rst') as file_object:
-  long_description = file_object.read()
-
-setup(
-  name = 'securesystemslib',
-  version = '0.18.0',
-  description = 'A library that provides cryptographic and general-purpose'
-      ' routines for Secure Systems Lab projects at NYU',
-  license = 'MIT',
-  long_description = long_description,
-  long_description_content_type = 'text/x-rst',
-  author = 'https://www.updateframework.com',
-  author_email = 'theupdateframework@googlegroups.com',
-  url = 'https://github.com/secure-systems-lab/securesystemslib',
-  keywords = 'cryptography, keys, signatures, rsa, ed25519, ecdsa',
-  classifiers = [
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Natural Language :: English',
-    'Operating System :: POSIX',
-    'Operating System :: POSIX :: Linux',
-    'Operating System :: MacOS :: MacOS X',
-    'Operating System :: Microsoft :: Windows',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: Implementation :: CPython',
-    'Topic :: Security',
-    'Topic :: Software Development'
-  ],
-  project_urls = {
-    'Source': 'https://github.com/secure-systems-lab/securesystemslib',
-    'Issues': 'https://github.com/secure-systems-lab/securesystemslib/issues',
-  },
-  python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4",
-  install_requires = ['six>=1.11.0', 'subprocess32; python_version < "3"'],
-  extras_require = {
-      'colors': ['colorama>=0.3.9'],
-      'crypto': ['cryptography>=2.6'],
-      'pynacl': ['pynacl>1.2.0']},
-  tests_require = 'mock; python_version < "3.3"',
-  packages = find_packages(exclude=['tests', 'debian']),
-  scripts = []
-)
+if __name__ == "__main__":
+  setup()
